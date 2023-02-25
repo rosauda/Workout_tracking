c5f62f75276b2f74a2d636324b163ae09b7c2e84
rodrigo.sauda <rodrigo.sauda@hotmail.com>
2023-02-25 20:24:07 +0100

    Updating gsheet

--- exercise_tracker.py
+++ exercise_tracker.py
@@ -1,5 +1,7 @@
 import os
 import requests
+from datetime import datetime
+
 
 # ---------------------------- VARIABLES ------------------------------- #
 
@@ -11,6 +13,10 @@ AGE = 32
 # Environment variables
 APP_ID = os.environ["api_id"]
 API_KEY = os.environ["api_key"]
+SHEET_ENDPOINT = os.environ["sheet_endpoint"]
+USERNAME = os.environ["username"]
+PASSWORD = os.environ["password"]
+
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
@@ -34,3 +40,26 @@ parameters = {
 response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
 print(result)
+
+# ---------------------------- UPDATING GSHEET ------------------------------- #
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    # Basic Authentication
+    sheet_response = requests.post(SHEET_ENDPOINT, json=sheet_inputs, auth=(USERNAME, PASSWORD))
+
+
+
+
